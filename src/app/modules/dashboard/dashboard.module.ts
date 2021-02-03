17efa3319d5af23ece4603995d1a99d15977fc79
Mayank Sethi <sethimayank97@gmail.com>
2021-02-03 10:57:39 +0530

    added dashboard UI

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import {MatCardModule} from '@angular/material/card'
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardHomeComponent } from './components/dashboard-home/dashboard-home.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 
 @NgModule({
   declarations: [DashboardHomeComponent],
   imports: [
     CommonModule,
-    DashboardRoutingModule
+    DashboardRoutingModule,
+    MatCardModule,
+    MatToolbarModule
   ]
 })
 export class DashboardModule { }
